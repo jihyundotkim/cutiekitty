b8295c3fde31f651746c46ab1d5117b07154045b
jihyundotkim <54026908+jihyundotkim@users.noreply.github.com>
2020-11-07 19:07:54 +0900

    Add files via upload
    Added comments.php

--- single.php
+++ single.php
@@ -9,13 +9,14 @@
 
                   <h2 class="center"> <?php									the_title(); ?></h2>
 <?php							the_post_thumbnail();?>
-								</a>
+</a>
                 <?php the_content(); ?>
-                <hr />
+                <hr /><h3>Comments</h3><?php comments_template(); comment_form();
+                ?>
                 <span class="prev-post"><?php previous_post_link(); ?><br /></span>
 <span class="next-post"><?php next_post_link(); ?></span><?php } else {?> <p class="center">No posts here!</p> <?php }?>
+
     </div>
-    <?php comment_form(); ?>
           <?php
 
 get_footer();
